de41c6c6de6208231b4713b1833831ecfdb2ec83
yujiye <yujiye@gxyongzhitai.com>
2025-06-05 22:55:46 +0800

    [feat] JdbcTemplate的CRUD

---
+++ spring-03-dao/src/main/java/com/github/yescco2d71828/jdbc/b_crud/config/JdbcTemplateCRUDConfiguration.java
@@ -0,0 +1,16 @@
+package com.github.yescco2d71828.jdbc.b_crud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class JdbcTemplateCRUDConfiguration {
+    @Bean
+    public DriverManagerDataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.sqlite.JDBC");
+        dataSource.setUrl("jdbc:sqlite:db/app.db");
+        return dataSource;
+    }
+}
